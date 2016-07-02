8d9d9d6cacde5a5a16b179eee5b5b97e7cb62603
Nathan LaFranchi <natelafranchi@gmail.com>
2016-07-02 10:42:36 -0700

    Added singly linked list

---
+++ linked-lists/singly-linked-list.js
@@ -0,0 +1,122 @@
+var SinglyLinkedList = (function() {
+
+	var Node = function(val, node) {
+		this.value = val;
+		this.next = node;
+	}
+	
+	function SinglyLinkedList() {
+		this.size = 0;
+		this.head = null;
+	}
+
+	SinglyLinkedList.prototype.isEmpty = function() {
+		return this.size == 0;
+	}
+
+	SinglyLinkedList.prototype.size = function() {
+		return this.size;
+	}
+
+	SinglyLinkedList.prototype.add = function(value) {
+		var temp = this.head;
+		this.head = new Node(value);
+		this.head.next= temp;
+		this.size++;
+	}
+
+	SinglyLinkedList.prototype.removeFirst = function() {
+		if (!this.head) {
+			console.log('Error: this list is empty!');
+			return;
+		}
+		var temp = this.head;
+		this.head = this.head.next;
+		temp.next = null;
+		this.size--;
+	}
+
+	SinglyLinkedList.prototype.removeLast = function() {
+		var temp = this.head;
+		if (!temp) {
+			console.log('Error: this list is empty!');
+			return null;
+		} else if (this.size == 1) {
+			this.head = null;
+			return temp;
+		} 
+
+		// Pointers for finding last node
+		// ref2 will ultimately be the last
+		// node in the list
+		var ref1 = this.head;
+		var ref2 = ref1.next;
+
+		while (ref2.next) {
+			ref1 = ref1.next;
+			ref2 = ref2.next;
+		}
+
+		ref1.next = null;
+		this.size--;
+		return ref2;
+	}
+
+	SinglyLinkedList.prototype.get(index) {
+		if(index >= 0) {
+			var count = 0;
+			while(count < index) {
+				if(temp.value == val) {
+					return 
+				}
+			}
+		}
+		
+	}
+
+	SinglyLinkedList.prototype.toString = function() {
+		var node = this.head;
+		while (node) {
+			console.log('a'  + node.value +  ' ' + this.size + (node.next ?  ', ' : ''));
+			node = node.next;
+		}
+	}
+
+	return SinglyLinkedList;
+
+})();
+
+
+var list = new SinglyLinkedList();
+var list2 = new SinglyLinkedList();
+
+list.add('100');
+list.add('111');
+list.add('222');
+
+list2.add('100111');
+list2.add('111111');
+list2.add('222111');
+
+list.removeLast();
+list2.removeFirst();
+
+console.log(list.toString());
+console.log(list2.toString());
+
+list.removeLast();
+list2.removeFirst();
+
+console.log(list.toString());
+console.log(list2.toString());
+list.removeLast();
+list2.removeFirst();
+
+console.log(list.toString());
+console.log(list2.toString());
+
+list.removeLast();
+list2.removeFirst();
+
+console.log(list.toString());
+console.log(list2.toString());
